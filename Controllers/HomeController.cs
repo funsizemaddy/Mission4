3cfd1dff7883a727a7bd839b62382e4ee29af616
funsizemaddy <90282709+funsizemaddy@users.noreply.github.com>
2022-01-25 19:08:32 -0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Mission4.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission4.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        // Podcasts is the action connected to the index page. 
+        //the View returns the html page we want to view. Name different for communication
+        //If the action is the same name as the html, you don't need to add the name in the View
+        public IActionResult Podcasts()
+        {
+            return View("Podcast");
+        }
+
+        [HttpGet]
+        public IActionResult Add_to_Movies()
+        {
+            return View("Movie_form");
+        }
+
+        [HttpPost]
+        public IActionResult Add_to_Movies(Movie_app_response mar)
+        {
+            return View("Conformation", mar);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Movie_app_response.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission4.Models
+{
+    public class Movie_app_response
+    {
+        public string Category { get; set; }
+        public string Title { get; set; }
+        public string Director { get; set; }
+        public string Rating { get; set; }
+        public bool Edited { get; set; }
+        public string Lent { get; set; }
+        public string Notes { get; set; }
+    }
+}
